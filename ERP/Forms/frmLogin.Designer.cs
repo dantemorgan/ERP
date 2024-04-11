22528c0f47ff3931e4e7f75b320824a99b58f911
dantemorgan <marcussantos.silveira@yahoo.com.br>
2024-04-10 21:35:56 -0300

    Primeito Commit

---
+++ ERP/Forms/frmLogin.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ERP.Forms
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUser = new TextBox();
+            txtSenha = new TextBox();
+            lblUser = new Label();
+            lblSenha = new Label();
+            btnEntrar = new Button();
+            btnSair = new Button();
+            SuspendLayout();
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(12, 87);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(285, 23);
+            txtUser.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(12, 141);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PasswordChar = '*';
+            txtSenha.Size = new Size(285, 23);
+            txtSenha.TabIndex = 1;
+            // 
+            // lblUser
+            // 
+            lblUser.AutoSize = true;
+            lblUser.Location = new Point(12, 65);
+            lblUser.Name = "lblUser";
+            lblUser.Size = new Size(50, 15);
+            lblUser.TabIndex = 2;
+            lblUser.Text = "Usuário:";
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Location = new Point(12, 123);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new Size(42, 15);
+            lblSenha.TabIndex = 3;
+            lblSenha.Text = "Senha:";
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(204, 182);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(92, 28);
+            btnEntrar.TabIndex = 4;
+            btnEntrar.Text = "Login";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            btnSair.Location = new Point(111, 183);
+            btnSair.Name = "btnSair";
+            btnSair.Size = new Size(87, 26);
+            btnSair.TabIndex = 5;
+            btnSair.Text = "Sair";
+            btnSair.UseVisualStyleBackColor = true;
+            btnSair.Click += btnSair_Click;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(327, 222);
+            Controls.Add(btnSair);
+            Controls.Add(btnEntrar);
+            Controls.Add(lblSenha);
+            Controls.Add(lblUser);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUser);
+            Name = "frmLogin";
+            Text = "frmLogin";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUser;
+        private TextBox txtSenha;
+        private Label lblUser;
+        private Label lblSenha;
+        private Button btnEntrar;
+        private Button btnSair;
+    }
+}
